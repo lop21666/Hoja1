5bc1e326e667c31df54fbb9169776a57900af624
lop21666 <91706525+lop21666@users.noreply.github.com>
2022-01-29 17:13:27 -0600

    Creación interfaz Radio

---
+++ src/Radio.java
@@ -0,0 +1,29 @@
+
+public interface Radio {
+
+	//Cambia el estado encendido/apagado
+	public void encenderApagar();
+	
+	//Guarda la emisora como favorita en el botón indicado (array de 12 espacios).
+	public String guardarEmisoraActual(int numBoton);
+	
+	//Reproduce la emisora guardada en uno de los 12 botones.
+	public String seleccionarEmisoraGuardada(int numBoton);
+	
+	//true: AM // false:FM
+	public String cambiarSenal(Boolean opcion);
+	
+	//true: AM // false:FM
+	public boolean getTipoSenal();
+	
+	// Aumenta la emisora actual en 0.2 si la señal actual es FM, o en 10 si es AM
+	public void subirEmisora();
+	
+	// Disminuye la emisora actual en 0.2 si la señal actual es FM, o en 10 si es AM
+	public void bajarEmisora();
+	
+	public float getEmisoraActual();
+	
+	public boolean comprobarEncendida();
+	
+}
